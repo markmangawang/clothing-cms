40d86a566d0c9a52cccf23164a1125014617e735
M. Mangawang <m.mangawang@pm.me>
2022-04-20 13:56:57 +0800

    Initialize firestore

--- src/routes/Sign-in/Sign-in.component.jsx
+++ src/routes/Sign-in/Sign-in.component.jsx
@@ -1,5 +1,20 @@
+import {
+  SignInWithGooglePopup,
+  firestore_userAuthDoc,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <h1>This is sign-in page.</h1>;
+  const userDataLog = async () => {
+    const response = await SignInWithGooglePopup();
+    firestore_userAuthDoc(response.user);
+  };
+  return (
+    <div className="container">
+      <h1>This is sign-in page.</h1>
+      <button onClick={userDataLog}>Sign-in with Google</button>
+    </div>
+  );
 };
 
 export default SignIn;
+
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, signInWithRedirect, signInWithPopup, getAuth } from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signInWithPopup,
+  getAuth,
+} from "firebase/auth";
+import { async } from "@firebase/util";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,10 +18,23 @@ const firebaseConfig = {
   projectId: "clothing-cms-db",
   storageBucket: "clothing-cms-db.appspot.com",
   messagingSenderId: "998579110859",
-  appId: "1:998579110859:web:11bd4e792f649a534d2c4e"
+  appId: "1:998579110859:web:11bd4e792f649a534d2c4e",
 };
-
-
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const SignInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const firestore_userDatabase = getFirestore();
+export const firestore_userAuthDoc = async (userAuth) => {
+  const firestore_userCreateDoc = await doc(firestore_userDatabase, 'users', userAuth.uid);
+  const firestore_userSnapshot = await getDoc(firestore_userCreateDoc);
+  console.log(firestore_userSnapshot);
+} 
